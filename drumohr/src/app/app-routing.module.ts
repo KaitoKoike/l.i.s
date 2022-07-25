1aa8c7e51c782b05f2cc2e58ae35e48a6163a8fc
KaitoKoike <kaikai.poteti@keio.jp>
2022-07-25 23:06:30 +0900

    ドルモアプロジェクトの追加

---
+++ drumohr/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TopPageComponent } from './top-page/top-page.component';
+
+const routes: Routes = [
+  {path:'TopPage',component:TopPageComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ drumohr/src/app/top-page/top-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '',
+  templateUrl: './top-page.component.html',
+  styleUrls: ['./top-page.component.css']
+})
+export class TopPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
