f510e0884dc57c3023f91268253b3f38fc8775e3
KaitoKoike <kaikai.poteti@keio.jp>
2022-07-26 00:57:00 +0900

    ヘッダーの完成

--- drumohr/src/app/app.module.ts
+++ drumohr/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, } from '@angular/platform-browser';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { GridModule } from '@angular/flex-layout';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopPageComponent } from './top-page/top-page.component';
 
+// フォーム作成用モジュール
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { CustomBreakPointsProvider, CustomLayoutDirective } from './custom-BREAKPOINTS';
+
 @NgModule({
   declarations: [
     AppComponent,
-    TopPageComponent
+    TopPageComponent,
+    CustomLayoutDirective
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    GridModule
   ],
-  providers: [],
+  providers: [CustomBreakPointsProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
