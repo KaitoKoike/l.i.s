4905eec223e56bed304140c3bc63b74f383cba14
KaitoKoike <kaikai.poteti@keio.jp>
2022-07-30 19:29:28 +0900

    drumohrの完成

---
+++ drumohr/src/app/shop-page/shop_list.ts
@@ -0,0 +1,164 @@
+
+export const SHOP_LIST = [{
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "伊勢丹メンズ館",
+    "post_cd": "160-0022",
+    "address": "東京都新宿区新宿3-14-1　5F メンズテーラードクロージング",
+    "gm_link": "https://goo.gl/maps/8J8F1UqvuU5N7cLTA",
+    "phone_num": "03-3352-1111"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "Chalk Stream",
+    "post_cd": "161-0033",
+    "address": "東京都新宿区下落合3-17-37",
+    "gm_link": "https://goo.gl/maps/iNxstRrfLE9RFeSV9",
+    "phone_num": "03-5982-7388"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "阪急メンズ東京",
+    "post_cd": "100-8488",
+    "address": "東京都千代田区有楽町2-5-1　5F　サードスタイル",
+    "gm_link": "https://g.page/hankyu_mens_tokyo?share",
+    "phone_num": "03-6252-1381"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "ピッコログランデ",
+    "post_cd": "150-0012",
+    "address": "東京都渋谷区広尾5-19-16 CASAS広尾 1F",
+    "gm_link": "https://goo.gl/maps/xjuAaBF1BxNuDaqC6",
+    "phone_num": "03-3444-1618"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "RE UNION",
+    "post_cd": "438-0071",
+    "address": "静岡県磐田市今之浦2-5-3",
+    "gm_link": "https://goo.gl/maps/uxjnHgFteELCrzC76",
+    "phone_num": "0538-33-0411"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "阪急メンズ大阪",
+    "post_cd": "530-0017",
+    "address": "大阪府大阪市北区角田町7-10 4F サードスタイル",
+    "gm_link": "https://goo.gl/maps/tbc5bHN2hWQUy2DA6",
+    "phone_num": "06-6361-1381"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "guji 大阪",
+    "post_cd": "604-0033",
+    "address": "大阪府大阪市北区梅田2-5-25 ハービスプラザ2F",
+    "gm_link": "https://goo.gl/maps/WB58NxcXCaoNsMUb7",
+    "phone_num": "075-708-5173"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "ITAL STYLE大阪京町堀店",
+    "post_cd": "550-0003",
+    "address": "大阪市西区京町堀1-17-16 京町堀センタービルディング1F",
+    "gm_link": "https://goo.gl/maps/UQU5JT8eykWRBKm57",
+    "phone_num": "06-6441-1922"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "ジェイアール京都伊勢丹",
+    "post_cd": "600-8555",
+    "address": "京都府京都市下京区烏丸通塩小路下ル東塩小路町 6F ISETAN MEN'S　紳士服 / 紳士洋品雑貨",
+    "gm_link": "https://goo.gl/maps/MzfhQxCGNq4sy5dG8",
+    "phone_num": "075-352-1111"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "PANNA 大名店",
+    "post_cd": "810-0041",
+    "address": "福岡県福岡市中央区大名2-2-1 MIKIビル 1F",
+    "gm_link": "https://goo.gl/maps/kCHJM1HtbPj2BADq8",
+    "phone_num": "092-716-2302"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "PANNA 博多店",
+    "post_cd": "812-0012",
+    "address": "福岡県福岡市博多区博多駅中央街1-1 AMU PLAZA博多 5F",
+    "gm_link": "https://goo.gl/maps/Bv9hUaiCVkGf1csu6",
+    "phone_num": "092-409-3917"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "BARNEYS NEW YORK",
+    "post_cd": "",
+    "address": "※各店舗にお問い合わせください",
+    "gm_link": "",
+    "phone_num": "0120-137-007"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "BEAMS F",
+    "post_cd": "",
+    "address": "※各店舗にお問い合わせください",
+    "gm_link": "",
+    "phone_num": "0120-011-301"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "ESTNATION",
+    "post_cd": "",
+    "address": "※各店舗にお問い合わせください",
+    "gm_link": "",
+    "phone_num": "03-5159-7800"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "SHIPS",
+    "post_cd": "",
+    "address": "※各店舗にお問い合わせください",
+    "gm_link": "",
+    "phone_num": "03-5524-2255"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "UNITED ARROWS",
+    "post_cd": "",
+    "address": "※各店舗にお問い合わせください",
+    "gm_link": "",
+    "phone_num": "0120-559-652"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "実店舗",
+    "shop_name": "高島屋 サロンルシックセレクト",
+    "post_cd": "",
+    "address": "※各店舗にお問い合わせください",
+    "gm_link": "",
+    "phone_num": "03-3211-4111"
+  },
+  {
+    "brand": "Drumohr",
+    "ec_or_shop": "WEB店舗",
+    "shop_name": "粋な着こなし 楽天市場店",
+    "post_cd": "",
+    "address": "https://www.rakuten.ne.jp/gold/ikinakikonashi/",
+    "gm_link": "",
+    "phone_num": "03-6457-6769"
+  }
+]
